ae1b7d8f4bdc9eed5a1a8dedcf3c696f96f2ec56
ShaneBell <sbell@nait.ca>
2026-02-09 09:49:08 -0700

    Feb 9

---
+++ list_loops_exercise/q3.py
@@ -0,0 +1,12 @@
+days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+days_of_weekend = ("Saturday", "Sunday")
+
+for index, day in enumerate(days_of_week, start = 1):
+    print (f"{day} is day number {index} of the week")
+    if day in days_of_weekend:
+        print(F"{day} is a weekend day")
+    else:
+        print(F"{day} is a weekday")
+
+
+
